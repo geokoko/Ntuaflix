bc5c7accc635e14f3511d085f79e62f5411cbe08
Konstantina Panou <72971678+ntua-el20144@users.noreply.github.com>
2024-02-07 21:33:58 +0200

    Added cli endpoints
    To help during testing

--- cli-client/cli.py
+++ cli-client/cli.py
@@ -23,6 +23,79 @@ def resetall():
         print(f"An error occurred: {e}")
 
 
+#Not tested yet
+def newtitles(args):
+    if not os.path.isfile(args.filename):
+        print(f"Error: File '{args.filename}' does not exist.")
+        return
+
+    try:
+        with open(args.filename, 'rb') as file:
+            files = {'file': (args.filename, file, 'application/octet-stream')}
+            response = requests.post(f'{BASE_URL}/admin/upload/titlebasics', files=files)
+            handle_response(response, args.format)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+#Not tested yet
+def newakas(args):
+    if not os.path.isfile(args.filename):
+        print(f"Error: File '{args.filename}' does not exist.")
+        return
+
+    try:
+        with open(args.filename, 'rb') as file:
+            files = {'file': (args.filename, file, 'application/octet-stream')}
+            response = requests.post(f'{BASE_URL}/admin/upload/titleakas', files=files)
+            handle_response(response, args.format)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+
+def newnames(args):
+    if not os.path.isfile(args.filename):
+        print(f"Error: File '{args.filename}' does not exist.")
+        return
+
+    try:
+        with open(args.filename, 'rb') as file:
+            files = {'file': (args.filename, file, 'application/octet-stream')}
+            response = requests.post(f'{BASE_URL}/admin/upload/namebasics', files=files)
+            handle_response(response, args.format)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+def newcrew(args):
+    if not os.path.isfile(args.filename):
+        print(f"Error: File '{args.filename}' does not exist.")
+        return
+
+    try:
+        with open(args.filename, 'rb') as file:
+            files = {'file': (args.filename, file, 'application/octet-stream')}
+            response = requests.post(f'{BASE_URL}/admin/upload/titlecrew', files=files)
+            handle_response(response, args.format)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+def newepisode(args):
+    if not os.path.isfile(args.filename):
+        print(f"Error: File '{args.filename}' does not exist.")
+        return
+
+    try:
+        with open(args.filename, 'rb') as file:
+            files = {'file': (args.filename, file, 'application/octet-stream')}
+            response = requests.post(f'{BASE_URL}/admin/upload/titleepisode', files=files)
+            handle_response(response, args.format)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
 def newprincipals(args):
     if not os.path.isfile(args.filename):
         print(f"Error: File '{args.filename}' does not exist.")
@@ -51,20 +124,19 @@ def newratings(args):
         print(f"An error occurred: {e}")
 
 
-def name(args):
+def title(args):
     try:
         params = {"format": args.format}
-        response = requests.get(f"{BASE_URL}/name/{args.nameid}", params=params)
+        response = requests.get(f"{BASE_URL}/title/{args.titleID}", params = params)
         handle_response(response, args.format)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
 
-#endpoint not working
-def title(args):
+def name(args):
     try:
         params = {"format": args.format}
-        response = requests.get(f"{BASE_URL}/title/{args.titleID}", params = params)
+        response = requests.get(f"{BASE_URL}/name/{args.nameid}", params=params)
         handle_response(response, args.format)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
@@ -94,7 +166,7 @@ def handle_response(response, format):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ntuaflix CLI")
-    parser.add_argument("scope", choices=["healthcheck", "resetall", "title", "name", "newprincipals","newratings"])
+    parser.add_argument("scope", choices=["healthcheck", "resetall", "title", "name", "newtitles", "newakas", "newnames", "newcrew", "newepisode", "newprincipals", "newratings"])
     parser.add_argument("--filename", help="Specify filename for newprincipals/newratings scope")
     
     parser.add_argument("--titleID", help="Specify titleID for title scope")
@@ -126,15 +198,26 @@ if __name__ == "__main__":
             print("Error: --titleID is a mandatory parameter for the 'title' scope.")
         else:
             title(args)
+    elif args.scope == "newtitles":
+        if not args.filename:
+            print("Error: --filename is a mandatory parameter for the 'newtitles' scope.")
+        else:
+            newtitles(args)  
+    elif args.scope == "newnames":
+        if not args.filename:
+            print("Error: --filename is a mandatory parameter for the 'newnames' scope.")
+        else:
+            newnames(args)  
+    elif args.scope == "newcrew":
+        if not args.filename:
+            print("Error: --filename is a mandatory parameter for the 'newcrew' scope.")
+        else:
+            newcrew(args)  
+    elif args.scope == "newepisode":
+        if not args.filename:
+            print("Error: --filename is a mandatory parameter for the 'newepisode' scope.")
+        else:
+            newepisode(args)  
     elif args.scope == "newprincipals":
         if not args.filename:
             print("Error: --filename is a mandatory parameter for the 'newprincipals' scope.")
-        else:
-            newprincipals(args)
-    elif args.scope == "newratings":
-        if not args.filename:
-            print("Error: --filename is a mandatory parameter for the 'newratings' scope.")
-        else:
-            newratings(args)
-    else:
-        print("Invalid scope")
